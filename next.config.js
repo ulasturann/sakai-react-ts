e21c00b6110239b14898b50af6c4e3339d36d67b
Ulaş Turan <ulasturan@Ulas-MacBook-Pro.local>
2022-10-24 10:37:23 +0300

    Add necessary assets.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  basePath: process.env.NODE_ENV === "production" ? "/sakai-react" : "",
+  publicRuntimeConfig: {
+    contextPath: process.env.NODE_ENV === "production" ? "/sakai-react" : "",
+    uploadPath:
+      process.env.NODE_ENV === "production"
+        ? "/sakai-react/upload.php"
+        : "/api/upload",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
